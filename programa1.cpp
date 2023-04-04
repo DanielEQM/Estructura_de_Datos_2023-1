42ecf61aac2c5c5fc0baeaf101888fa6728beb1c
MatiFFuwu <matias.fernandezf@usm.cl>
2023-04-03 23:04:12 -0400

    consideraciones basicas

--- programa1.cpp
+++ programa1.cpp
@@ -2,3 +2,17 @@
 #include <string>
 
 using namespace std;
+
+int const SERV_DESAYUNO = 0;
+int const SERV_ALMUERZO = 1;
+int const SERV_ONCE = 2;
+int const SERV_CENA = 3;
+
+struct SaldoColaborador {
+    string rut ;
+    int saldo_desayuno ;
+    int saldo_almuerzo ;
+    int saldo_once ;
+    int saldo_cena ;
+}
+
