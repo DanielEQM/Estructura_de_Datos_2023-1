b130d0641b73837fa46b483ddae9be3b1edc3ae3
DanielEQM <116672987+DanielEQM@users.noreply.github.com>
2023-04-14 03:00:57 -0400

    algo xd

--- programa2.cpp
+++ programa2.cpp
@@ -26,10 +26,14 @@ int* comprarTarjeta(string nombre, int dia, int &m){
     int* ficha = new int[m];
     for (int i = 0; i < m; i++){
         ficha[i] = (int) nombre[i] % dia;
-        //cout << ficha[i]<< "\n";
     }
+    for(int i = 0; i<m; i++){
+        cout<<ficha[i]<<" ";
+    }
+    cout<<"\n";
     return ficha;
 }
+
 void intercambiarTarjeta(Persona*p1, Persona*p2){
     int* swap = p1->tarjeta;
     p1->tarjeta = p2->tarjeta;
@@ -37,53 +41,84 @@ void intercambiarTarjeta(Persona*p1, Persona*p2){
     p1->quiere_intercambiar = 0;
 }
 
+
 int puntaje(Persona* p1){
-    int contador,numero;
+    int contador = 0,numero;
     for(int i=0;i<p1->tamanio_tarjeta-1;i++){
-        numero=p1->tarjeta[i]*p1->fecha[i%10];
+        numero= p1->tarjeta[i]*p1->fecha[i%10];
         contador+=numero;
     }
     return contador;
 }
 
-Persona* unDia(Persona* personas,int dia){
-    int *tar[cantidad];
-    for(int i=0;i<cantidad;i++){
-        tar[i]=comprarTarjeta(personas->nombre,dia,cantidad)
-    }
-    
-}
-
-int main(){
-    int m;
-    //int dias = 2;
-    int *ficha;
-    cin>> cantidad;
-    Persona per[cantidad];
-    for (int i=0; i<cantidad; i++){ 
-        cin >> per[i].nombre >> per[i].fecha >> per[i]. quiere_intercambiar;
-    }
-    for (int i=0; i<cantidad; i++){
-        ficha = comprarTarjeta(per[i].nombre, 2, m);
-        per[i].tarjeta = ficha;
-        delete[] ficha;
-    }
-    for (int i=0; i<cantidad; i++){
-        if (per[i].quiere_intercambiar != 0){
-            for (int j = i + 1; j < cantidad; j++){
-                if (per[j].quiere_intercambiar != 0){
-                    intercambiarTarjeta(&per[i], &per[j]);
-                    break;
-                }
+bool comun(Persona *p1, Persona *p2){
+    int a = (p1->nombre).length();
+    int b = (p2->nombre).length();
+    for (int i=0; i< a;i++){
+        for (int j = 0; j < b; j++){
+            if (p1->tarjeta[i] == p2->tarjeta[j]){
+                return 1;
             }
         }
     }
     return 0;
 }
-/* 
-Demuestra que es el arreglo correspondiente
-    for (int j= 0; j < 7;j++){
-        cout << (per[i].tarjeta)[j] << " ";
+
+Persona* unDia(Persona* personas,int dia){
+    dia = 1;
+    int m;
+    bool X;
+    int menor = -1;
+    int puntos;
+    int win;
+    for (int i=0; i<cantidad; i++){
+        personas[i].tarjeta = comprarTarjeta(personas[i].nombre, 1, m);
+        personas[i].tamanio_tarjeta = m;
     }
-    cout<<"\n";
-*/
+    for (int i=0; i<cantidad; i++){
+        if (personas[i].quiere_intercambiar != 0){
+            for (int j = i + 1; j < cantidad; j++){
+                if (personas[j].quiere_intercambiar != 0){
+                    X = comun(&personas[i], &personas[j]);
+                    if (X == 1){    
+                        intercambiarTarjeta(&personas[i], &personas[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i<cantidad; i++){
+        puntos = puntaje(&personas[i]);
+        if(menor < puntos){
+            menor = puntos;
+            win = i;
+        }
+    }
+    return &personas[win];
+}
+
+void variosDias(Persona* personas, int cant_dias){
+    Persona *pers;
+    for(int i= 1; i < cant_dias + 1; i++){
+        for(int j=0; j < cantidad; j++){
+            pers = unDia(personas, i);
+        }
+        cout<<pers->nombre << " "<< pers->fecha << " " << puntaje(pers) << "\n";
+        for (int i = 0; i <cantidad; i++){
+            delete[] personas[i].tarjeta;
+        }
+    }
+}
+
+
+
+int main(){
+    cin>> cantidad;
+    Persona per[cantidad];
+    for (int i=0; i<cantidad; i++){ 
+        cin >> per[i].nombre >> per[i].fecha >> per[i]. quiere_intercambiar;
+    }
+    variosDias(per, 2);
+    return 0;
+}
