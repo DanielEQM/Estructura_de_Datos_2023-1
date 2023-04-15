d556c267b7e2647f4b795a83c0e4881ae7bb8d76
DanielEQM <116672987+DanielEQM@users.noreply.github.com>
2023-04-14 22:19:17 -0400

    Ya pero lucas, abriste el archivo?

--- programa1.cpp
+++ programa1.cpp
@@ -19,8 +19,27 @@ struct SaldoColaborador {
 
 bool puedeConsumir(char* rut, int servicio, string consumos_dia){
     return 0;
-};
+}
 
+int main(){
+    ifstream fp;
+    int n;
+    fp.open("saldos.bin", ios::binary);
+    if (!fp.is_open()){
+        cerr << "Error el abrir el archivo" << "\n";
+        return 1;
+    }
+    fp.read((char*)&n, sizeof(int));
+    SaldoColaborador* arr = new SaldoColaborador[n];
+    fp.read((char*)arr, sizeof(SaldoColaborador) * n);
+    for (int i = 0; i < n; i++){
+        cout << arr[i].rut << " " << arr[i].saldo_almuerzo << " " << arr[i].saldo_cena << " " << arr[i].saldo_desayuno << " " << arr[i].saldo_once << "\n";
+    }
+    fp.close();
+    return 0;
+}
+
+/*
 int main(){
     int n;
     int rec;
@@ -44,5 +63,6 @@ int main(){
     file.close();
     return 0;
 };
+*/
 
 // matigod
